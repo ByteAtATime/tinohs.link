7755686b7c02b49b6662a50275825e5d3567afa9
ByteAtATime <byteatatime@proton.me>
2024-12-03 18:10:04 -0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import { ClerkProvider } from 'clerk-sveltekit/client';
+	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+
+	let { children } = $props();
+</script>
+
+<ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	{@render children()}
+</ClerkProvider>
