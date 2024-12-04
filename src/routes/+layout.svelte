4828e7e431c84fdd65a64c04b084235cbb9bd1f6
ByteAtATime <byteatatime@proton.me>
2024-12-04 11:19:06 -0800

    feat: add shadcn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,13 @@
 	import '../app.css';
 	import { ClerkProvider } from 'clerk-sveltekit/client';
 	import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+	import { Toaster } from "$lib/components/ui/sonner";
 
 	let { children } = $props();
 </script>
 
 <ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	<Toaster />
+	
 	{@render children()}
 </ClerkProvider>
