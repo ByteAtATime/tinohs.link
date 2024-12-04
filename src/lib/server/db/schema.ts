3f1f34c4bfc17d0ed50ed9c8b3f2f62930f416f5
ByteAtATime <byteatatime@proton.me>
2024-12-04 11:02:39 -0800

    feat: add users table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+
+export const authProviderEnum = pgEnum('auth_provider', ["clerk"]);
+
+export const usersTable = pgTable('users', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	authProvider: authProviderEnum('auth_provider').notNull(),
+	authProviderId: text('auth_provider_id').notNull(),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
+	updatedAt: timestamp('updated_at').notNull().defaultNow()
+});
 
 export const urlMappings = pgTable('url_mappings', {
 	id: text('id').primaryKey(),
