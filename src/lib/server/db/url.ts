bf29ee73a3f9f33606add341a8ff9987146210de
ByteAtATime <byteatatime@proton.me>
2024-12-03 18:50:12 -0800

    feat: drizzle-zod for data verification

--- src/lib/server/db/url.ts
+++ src/lib/server/db/url.ts
@@ -1,6 +1,14 @@
 import { db } from '$lib/server/db';
 import { urlMappings } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
+
+export const selectUrlSchema = createSelectSchema(urlMappings);
+export type SelectUrlSchema = z.infer<typeof selectUrlSchema>;
+
+export const insertUrlSchema = createInsertSchema(urlMappings);
+export type InsertUrlSchema = z.infer<typeof insertUrlSchema>;
 
 export const getURLFromShortPath = async (shortPath: string) => {
 	const results = await db.select({ redirect: urlMappings.redirectUrl }).from(urlMappings).where(eq(urlMappings.shortUrl, shortPath));
