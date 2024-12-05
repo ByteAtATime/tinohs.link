96a22ba432a7e5243ebb3e102777167775c5aad4
ByteAtATime <byteatatime@proton.me>
2024-12-05 12:13:44 -0800

    chore: remove unneeded imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { PUBLIC_ROOT_URL } from '$env/static/public';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
 	import { Input } from '$lib/components/ui/input';
